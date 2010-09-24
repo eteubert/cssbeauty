a375f3d0d93588bbe637011d037dc549304ae543
Eric Teubert <ericteubert@googlemail.com>
2010-09-24 18:44:35 +0200

    make parser extract selectors

--- lib/css/parser.rb
+++ lib/css/parser.rb
@@ -1,7 +1,20 @@
 module CSS
   class Parser
     def parse(css)
-      return [CSS::Rule.new(:selector => CSS::Selector.new(:raw => "body > .class a"))]
+      result = []
+      
+      rules = css.split("}")
+      
+      # remove last rule if it is empty
+      rules.pop if rules.last.count("{") == 0
+      
+      # iterate over each rule
+      rules.each do |rule|
+        selector, attributes = rule.split("{")
+        result << CSS::Rule.new(:selector => CSS::Selector.new(:raw => selector.strip))
+      end
+      
+      return result
     end
   end
 end
