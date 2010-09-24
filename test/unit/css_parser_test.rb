a05b5f8094fd10f3838e781b0196ed2dd9e4c0e5
Eric Teubert <ericteubert@googlemail.com>
2010-09-24 18:02:05 +0200

    create setup method for tests

--- test/unit/css_parser_test.rb
+++ test/unit/css_parser_test.rb
@@ -2,11 +2,19 @@ require 'test_helper'
 require "css/parser"
 
 class CssParserTest < ActiveSupport::TestCase
+  
+  def setup
+    @parser = CSS::Parser.new
+    @css = <<-EOT
+      body > .class a { } html, body, p { margin: 0px; }
+      p { padding: 0px; }
+      #content { font: 12px/normal sans-serif; }
+    EOT
+  end
+  
   # Replace this with your real tests.
   test "parser finds css selector in one rule" do
-    css = "body > .class a { }";
-    parser = CSS::Parser.new
-    result = parser.parse(css)
+    result = @parser.parse(@css)
     
     assert_instance_of(CSS::Rule, result.first)
     assert_equal("body > .class a", result.first.selector.raw)
