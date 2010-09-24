8e2bc9376bb248719bd37fedf15399a6f31c3c90
Eric Teubert <ericteubert@googlemail.com>
2010-09-24 16:52:47 +0200

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cssbeauty::Application.config.secret_token = '1cc6c1e6fcea0a620eb27078811443d9dc6e182749c149950d3950f3e21ee3b9237cc5d5487b17fb4a5d2fb0cdd5a1070e5995325829ea9729165b095d64dd6d'
