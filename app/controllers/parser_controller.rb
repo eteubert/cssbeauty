eb56a471465acea21f1af889d41b19657f5d4d81
Eric Teubert <ericteubert@googlemail.com>
2010-09-24 21:03:37 +0200

    create basic layout

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -4,11 +4,13 @@ require "css/printer"
 class ParserController < ApplicationController
   def new
     @css = ""
+    @rows = 20
   end
 
   def create
     @parsed = CSS::Parser.new.parse(params[:css])
     @css = CSS::Printer.new.format_all(@parsed)
+    @rows = @css.count("\n") + 1
     render :new
   end
 
